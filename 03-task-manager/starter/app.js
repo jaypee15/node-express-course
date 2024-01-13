67e5d225abed4a5c685a741eca1a5f683880a3ef
jaypee15 <okoyejohnpaul15@gmail.com>
2024-01-13 23:59:11 +0100

    feat: add asyncwarpper and custom error handling

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,32 +1,30 @@
-require('./db/connect')
-const express = require('express')
-const app = express()
-const tasks = require('./routes/tasks')
-const connectDB = require('./db/connect')
-require('dotenv').config()
-
-
+require("./db/connect");
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+require("dotenv").config();
 
 // middlewares
-app.use(express.json())
+app.use(express.static("./public"));
+app.use(express.json());
 
 // routes
-app.get('/hello', (req, res)=>{
-    res.end('Hello World')
-})
+app.use("/api/v1/tasks", tasks);
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
-app.use('/api/v1/tasks', tasks)
-
-const port = 3000
+const port = 3000;
 const start = async () => {
-    try{
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, ()=>{
-            console.log(`Server is listening on port ${port}...`)
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
+start();
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,30 +1,51 @@
+const Task = require("../models/tasks");
+const asyncWrapper = require("./../middleware/async");
 
-const getAllTasks = (req, res) => {
-    res.send('get all tasks')
-}
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
 
-const createTask = (req, res) => {
-    res.json(req.body)
-    
-}
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
 
-const getTask = (req, res) => {
-    res.json({id:req.params.id})
-    
-}
+const getTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
 
-const updateTask = (req, res) => {
-    res.send('update tasks')
-}
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+  }
+  res.status(200).json({ task });
+});
 
-const deleteTask = (req, res) => {
-    res.send('delete tasks')
-}
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!task) {
+    return res.status(404).json({ msg: `task with id ${taskID} not found` });
+  }
+  res.status(200).json({ task });
+});
+
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    return res.status(404).json({ msg: `task with id ${taskID} not found` });
+  }
+  res.status(204).json({ msg: "deleted succesfully" });
+});
 
 module.exports = {
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask
-}
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
